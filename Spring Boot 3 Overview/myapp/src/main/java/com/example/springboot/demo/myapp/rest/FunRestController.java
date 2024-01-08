bfa871f5ba74a7fd5725ca6ea9d205739a9a43a1
Toan vip pro <ktoan515@gmail.com>
2024-01-08 23:39:07 +0700

    end 01 Spring Boot 3 Overview

--- Spring Boot 3 Overview/myapp/src/main/java/com/example/springboot/demo/myapp/rest/FunRestController.java
+++ Spring Boot 3 Overview/myapp/src/main/java/com/example/springboot/demo/myapp/rest/FunRestController.java
@@ -1,13 +1,20 @@
 package com.example.springboot.demo.myapp.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String sayHello(){
-        return "Hello Spring Boot";
+        return "TeamName: " + this.teamName +"; Coach Name: "+ this.coachName;
     }
 }
